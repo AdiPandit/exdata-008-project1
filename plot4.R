092a16a6af6c21a72f172ccb182fb975ba1a13fd
Adi Pandit <adi.pandit@gmail.com>
2014-11-09 19:09:03 -0500

    Uncomment the download file code

--- plot4.R
+++ plot4.R
@@ -1,6 +1,7 @@
+#Download and unzip the data
 fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-#download.file(fileUrl, dest = "household_power_consumption.zip", method="curl")
-#unzip("household_power_consumption.zip")
+download.file(fileUrl, dest = "household_power_consumption.zip", method="curl")
+unzip("household_power_consumption.zip")
 powerConsumptionData <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors = FALSE) 
 #Adjust time to incorporate correct date from the Date column
 powerConsumptionData <- transform(powerConsumptionData,  Time = paste(Date,Time,sep=" "))
